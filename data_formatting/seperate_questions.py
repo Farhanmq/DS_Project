d16e138dfd9a09a0674c995e3a03f54bd16efcaf
Jannik Maximilian Müller <Jannik.Mueller@dfki.de>
2025-05-23 01:35:01 +0200

    question seperation function

---
+++ data_formatting/seperate_questions.py
@@ -0,0 +1,85 @@
+import os.path
+import pandas as pd
+from slugify import slugify
+
+
+def create_table_per_question(filepath, sheet_name, folder_path):
+    table =pd.read_excel(filepath,sheet_name=sheet_name,header=None).fillna(method='ffill',axis=1)
+    # print(table)
+
+    #get indicies of nan rows
+    nan_rows = table.isna().all(axis=1)
+    nan_rows = nan_rows.where(cond=(nan_rows==True)).dropna()
+    # print(nan_rows)
+
+    #init question_tables dict
+    question_tables = dict()
+
+    # read tables
+    last_index=-1
+    second_last_index=-2
+    current_question=""
+
+    for i in range(len(nan_rows)):
+        if last_index != nan_rows.index[i] -1 and second_last_index == last_index -1:
+            # read question
+            question_raw = table.iloc[last_index+1:nan_rows.index[i],:]
+
+            if current_question != question_to_string(question_raw):
+                # New question gets added to the dictionary
+                current_question = question_to_string(question_raw)
+                question_tables[current_question] = []
+
+            # print(question_to_string(question_raw))
+            # print(question_raw)
+
+
+        elif last_index == nan_rows.index[i] -1:
+            # add table to question list
+            question_tables[current_question].append(pd.DataFrame(table.iloc[second_last_index+1:last_index,:]).reset_index(drop=True))
+            # print("table")
+
+        # update indices
+        second_last_index = last_index
+        last_index = nan_rows.index[i]
+
+    # combine tables for each question
+    for question in question_tables.keys():
+        for i in range(len(question_tables[question])):
+            if i != 0:
+                # remove duplicate columns
+                question_tables[question][i] = question_tables[question][i].iloc[:,2:]
+
+        # concat question tables to one big question table
+        question_tables[question] = pd.concat(question_tables[question],axis=1)
+        question_tables[question].columns = question_tables[question].iloc[0]
+        question_tables[question] = question_tables[question].iloc[1:,:]
+
+    # save table for each question
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    for question in question_tables.keys():
+        filename = slugify(question)
+        if len(filename) > 250:
+            filename = filename[:250]
+        question_tables[question].to_csv(os.path.join(folder_path,filename+".csv"),index=False)
+
+    # print(question_tables)
+
+
+def question_to_string(question_table: pd.DataFrame) -> str:
+    """
+    Converts a question table into a question string
+    :param question_table: The table, in which the question is included (over multiple rows)
+    :return: The question string
+    """
+    question_string = ""
+    for i in range(len(question_table)):
+        if question_string != "":
+            question_string += " | "
+        question_string += str(question_table.iloc[i,0])
+    return question_string
+
+if __name__ == '__main__':
+    create_table_per_question("../provided_data/Kundenmonitor_GKV_2023.xlsx", "Band", "/formatted_data/Kundenmonitor_GKV_2023/Band")
