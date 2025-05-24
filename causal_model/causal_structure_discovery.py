4e40b04d9ff6a65fb8a585f5ea48c962e7bb7a47
Jannik Maximilian Müller <Jannik.Mueller@dfki.de>
2025-05-24 16:58:14 +0200

    added documentary

---
+++ causal_model/causal_structure_discovery.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+
+def define_random_variables_for_question_table(question_table:pd.DataFrame)-> np.random:
+    # find labels and probabilities per label of total distribution of question
+
+
+def define_random_variables_for_question_tables(question_tables:list[str]):
--- data_formatting/seperate_questions.py
+++ data_formatting/seperate_questions.py
@@ -8,8 +8,14 @@ from numpy.ma.core import shape
 # from slugify import slugify
 
 
-def create_table_per_question(filepath, sheet_name, folder_path):
-    table =pd.read_excel(filepath,sheet_name=sheet_name,header=None)
+def create_table_per_question(filepath_data, sheet_name, folder_path_questions):
+    """
+    Creates tables for each question of a sheet  of an excel file and saves them in a folder.
+    :param filepath_data: The filepath of the excel file containing the data.
+    :param sheet_name: The name of the sheet in the excel file.
+    :param folder_path_questions: The folder path containing the resulting question tables.
+    """
+    table =pd.read_excel(filepath_data, sheet_name=sheet_name, header=None)
     # print(table)
 
     #get indicies of nan rows
@@ -63,8 +69,8 @@ def create_table_per_question(filepath, sheet_name, folder_path):
         question_tables[question] = question_tables[question].iloc[1:, :]
 
     # save table for each question
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
+    if not os.path.exists(folder_path_questions):
+        os.makedirs(folder_path_questions)
 
     i = 1
     question_list = []
@@ -74,7 +80,7 @@ def create_table_per_question(filepath, sheet_name, folder_path):
         #     filename = filename[:250]
         filename = "Question_" + str(i)
         question_list.append(filename)
-        question_tables[question].to_csv(os.path.join(folder_path, filename+".csv"),  index=False)
+        question_tables[question].to_csv(os.path.join(folder_path_questions, filename + ".csv"), index=False)
         i+=1
 
     # questions = np.empty(shape=(len(question_list),2))
@@ -83,14 +89,14 @@ def create_table_per_question(filepath, sheet_name, folder_path):
     # write questions table
     question_table =pd.DataFrame({"Question Nr":question_list,"Question": question_tables.keys() },
                                  columns=["Question Nr","Question"])
-    question_table.to_csv(os.path.join(folder_path, "question_table.csv"), index=False)
+    question_table.to_csv(os.path.join(folder_path_questions, "question_table.csv"), index=False)
 
 
 def question_to_string(question_table: pd.DataFrame) -> str:
     """
-    Converts a question table into a question string
-    :param question_table: The table, in which the question is included (over multiple rows)
-    :return: The question string
+    Converts a question table into a question string.
+    :param question_table: The table, in which the question is included (over multiple rows).
+    :return: The question string.
     """
     question_string = ""
     for i in range(len(question_table)):
@@ -101,6 +107,10 @@ def question_to_string(question_table: pd.DataFrame) -> str:
 
 
 def find_faulty_tables(folder_path):
+    """
+    Prints the filenames of all faulty question tables in a folder.
+    :param folder_path: The folder path to the question tables.
+    """
     for file in os.listdir(folder_path):
         df = pd.read_csv(os.path.join(folder_path, file))
         if file != "question_table.csv" and df.columns[1] != "Gesamt":
