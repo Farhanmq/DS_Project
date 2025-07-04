13e16ba2998713a2c8d406786b4da7b59be9893b
Jannik Maximilian Müller <Jannik.Mueller@dfki.de>
2025-07-04 22:57:00 +0200

    swithcing personas

--- baseline_models/test3.py
+++ baseline_models/test3.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
+from sklearn.tree._tree import Tree
 
 # Step 1: Load data
 df = pd.read_excel("Mapped_Result.xlsx")
@@ -118,7 +119,102 @@ plt.title("Decision Tree")
 plt.show()
 
 # Optional: Visualize the Decision Tree
-plt.figure(figsize=(200, 100))
+plt.figure(figsize=(100, 80))
 plot_tree(tree, feature_names=X.columns, class_names=["No Switch", "Switch"], filled=True)
 plt.title("Decision Tree Structure")
 plt.show()
+
+def get_leaf_paths(tree: Tree, node, node_path, features_path, thresholds_path, small_eq_thres_path) -> list:
+    """
+    Iterate through tree to receive paths to leaf nodes
+    :param tree: The tree object
+    :param node: The currently visited node.
+    :param node_path: The path to the node excluding the node itself.
+    :param features_path: The path with features to the node excluding the node itself.
+    :param thresholds_path: The path with thresholds to the node excluding the node itself.
+    :param small_eq_thres_path: Whether value in path was smaller or equal threshold value.
+    :return: [(leaf node id, path, features_path, thresholds_paths)]
+    """
+    path_to_node = node_path + [node]
+    features_path_to_node = features_path + [tree.feature[node]]
+    thresholds_path_to_node = thresholds_path + [tree.threshold[node]]
+    if tree.children_left[node] == -1:
+        # leaf node
+        return [(node, path_to_node, features_path_to_node, thresholds_path_to_node,small_eq_thres_path)]
+    else:
+        return (get_leaf_paths(tree, tree.children_left[node], path_to_node, features_path_to_node, thresholds_path_to_node, small_eq_thres_path + [True])
+                + get_leaf_paths(tree, tree.children_right[node], path_to_node, features_path_to_node, thresholds_path_to_node, small_eq_thres_path + [False]))
+
+tree_struct = tree.tree_
+
+leaf_paths = get_leaf_paths(tree_struct, 0, [],[],[],[])
+# print(leaf_paths)
+
+
+# get demographics and other values of switching leaf nodes
+leaf_node_demographics = []
+leaf_node_others = []
+for (leaf_node, leaf_path, leaf_feature_path, leaf_thresholds_path, small_eq_threshold_path) in leaf_paths:
+    if tree_struct.value[leaf_node][0][1] > tree_struct.value[leaf_node][0][0]:
+        # Switching behaviour leaf
+        feature_text = [list(X.columns)[leaf_feature_path[i]] for i in range(len(leaf_feature_path)-1)]
+
+        demographic_threshold_values = []
+        other_threshold_values = []
+        for i in range(len(feature_text)):
+            if feature_text[i] in demographics:
+                demographic_threshold_values.append((feature_text[i], leaf_thresholds_path[i], small_eq_threshold_path[i]))
+
+            else:
+                other_threshold_values.append((feature_text[i], leaf_thresholds_path[i], small_eq_threshold_path[i]))
+
+        leaf_node_demographics.append(demographic_threshold_values)
+        leaf_node_others.append(other_threshold_values)
+
+
+def print_switching_factor(switching_factor_name, switching_factor_threshold, less_eq_threshold):
+    print_str = switching_factor_name + " "
+    if less_eq_threshold:
+        print_str += "<= "
+    else:
+        print_str += "> "
+    print(print_str + str(switching_factor_threshold))
+
+print("")
+print("Found switching personas:")
+print("")
+
+for i in range(len(leaf_node_demographics)):
+    if len(leaf_node_demographics[i]) > 0:
+        print("Switching Persona:")
+        for j in range(len(leaf_node_demographics[i])):
+            print_switching_factor(*leaf_node_demographics[i][j])
+
+        print("")
+        print("Other Decision Factors of Persona:")
+        for j in range(len(leaf_node_others[i])):
+            print_switching_factor(*leaf_node_others[i][j])
+
+        print("")
+        print(f"Decision Purity: {100 * (1 -tree_struct.impurity[leaf_paths[i][0]])}")
+        print("")
+
+print("Further found switching patterns:")
+print("")
+
+for i in range(len(leaf_node_demographics)):
+    if len(leaf_node_demographics[i]) == 0:
+        print("Switching Pattern:")
+        for j in range(len(leaf_node_others[i])):
+            print_switching_factor(*leaf_node_others[i][j])
+
+        print("")
+        print(f"Decision Purity: {100 * (1 -tree_struct.impurity[leaf_paths[i][0]])}")
+        print("")
+
+print("Label values taken from the survey file.")
+print("The higher the purity, the more meaningful the found pattern (based on the survey data).")
+
+
+# print(leaf_node_demographics)
+# print(leaf_node_others)
